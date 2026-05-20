713b24e5a5a39af538ee7a4d995965f8d1884d39
GuilhermeDeVeras <guilhermepinheirodeveras@gmail.com>
2026-05-20 10:43:26 -0300

    fix: add missing case to print the set's numbers

--- set/main/main.c
+++ set/main/main.c
@@ -5,14 +5,14 @@
 
 int main(){
 
-	int i = 1, element, position;
+	int i = 1, element, position, x;
 	
 	set* s = NULL;
 	
 	s = create_set();
 	
 	while (i != 0) {
-		printf("1: Add an element\n2: Remove an element\n3: Search an element\n0: Stop operations\n\nDecision choosen: ");
+		printf("1: Add an element\n2: Remove an element\n3: Search an element\n4: Print the elements\n0: Stop operations\n\nDecision choosen: ");
 		scanf("%d", &i);
 		switch (i) {
 			case 1:
@@ -39,6 +39,13 @@ int main(){
 				scanf("%d", &element);
 				if (search_set(s, element)) printf("The element is in the set\n\n");
 				else printf("The element isn't in the set\n\n");
+				break;
+			case 4:
+				for (begin_set (s); !end_set (s); next_set (s)) {
+					get_item_set (s, &x);
+					printf("%d ", x);
+				}
+				printf("\n\n");
 				break;
 			case 0:
 				free_set(s);
